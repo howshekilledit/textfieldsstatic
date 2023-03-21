2858742db28814d57d6a26d2fb5fbc287f5f2b82
Morgan Green <cellardoor.morgan@gmail.com>
2023-03-21 13:37:36 -0500

    Add files via upload

--- medusa/sketch.js
+++ medusa/sketch.js
@@ -14,6 +14,7 @@ let red_left = false; //direction of red hair motion
 //note that right now width is the range for control points
 //and height is the range for the bezier curve full length
 function setup() {
+  document.body.style.zoom = 1.0;
 
   //svg canvas to draw on with svg.js/p5.js
 
@@ -38,7 +39,7 @@ function setup() {
   red_hair.gen_curves(0);
   red_hair.gen_circles();
   window.scrollTo(0, 0); //scroll to top left of page
-  toggle_pan(); //first call, as of now needs to calls to work
+  //toggle_pan(); //first call, as of now needs to calls to work
   //toggle_pan();
   frameRate(12);
   //set i_lists to two shuffled lists of indices
@@ -54,7 +55,11 @@ function setup() {
     red_left = true;
     //toggle_pan();
   }
+  if ((floor(random(2)) == 1) && instructions.style.display != 'block') {
+    toggle_pan();
+  }
   document.body.style.backgroundColor = 'black';
+
   //red_hair.draw_all_circles(cvs);
   //hair.draw_all_circles(cvs);
 
@@ -68,25 +73,36 @@ function setup() {
 
 
 function draw() {
-
-  if (frameCount < hair.circles.length) {
+  if(instructions.style.display == 'block'){
+    noLoop();
+  } else {
+    loop();
+  }
+  if (frameCount%(hair.circles.length*2) < hair.circles.length) { //while hair is growing
 
     if (panning) {
       svg.style.left = -frameCount % windowWidth;
     }
-    let ellipse = hair.draw_circle(cvs, frameCount);
-    let r_loc = frameCount;
-    if (red_left) { r_loc = hair.circles.length - frameCount; }
-    let red_ellipse = red_hair.draw_circle(cvs, r_loc, '#f00', '#000');
+    let ellipse = hair.draw_circle(cvs, frameCount%(hair.circles.length*2));
+    let r_loc =frameCount%(hair.circles.length*2)-1;
+    if (red_left) { r_loc = hair.circles.length - frameCount%(hair.circles.length*2)-1; }
+    let red_ellipse;
+    try{
+      red_ellipse = red_hair.draw_circle(cvs, r_loc, '#f00', '#000');
+      red_ellipse.mouseover(function () {
+        red_hair.writhe(2);
+      });
+    } catch(e){
+      console.log(e);
+      console.log(r_loc);
+    }
     ellipse.mouseover(function () {
       hair.writhe(2);
     });
-    red_ellipse.mouseover(function () {
-      red_hair.writhe(2);
-    });
-  } else {
+
+  } else { //when hair is done growing
     if (panning) {
-      svg.style.left += 1;
+      toggle_pan();
     }
     //noLoop();
     //if(getFrameRate() != 6){frameRate(6)}
@@ -95,18 +111,22 @@ function draw() {
     red_hair.writhe(end_writhe);
     hair.writhe(end_writhe);
     //if(frameCount % 2 == 0){
-    hair.circle_fades(bi, 100);
+    hair.circle_fades(bi, 40);
     // } else {
-    red_hair.circle_fades(ri, 100);
+    red_hair.circle_fades(ri, 40);
     // }
     end_writhe += 0.005;
     i++;
   }
+  if(frameCount%hair.circles.length*2 == hair.circles.length*2-1){
+    noLoop();
+    cvs.remove();
+    end_writhe = 0.2;
+    i=0;
+    setup();
+    loop();
+  }
 }
-
-// function mousePressed(){
-//   toggle_pan();
-// }
 
 function keyTyped() {
   console.log(key);
@@ -119,11 +139,11 @@ function keyTyped() {
       hair.update_colors(true); //update circles
       break;
     }
-    case 'g': {
-      let fll = red_hair.stroke;
-      let strk = red_hair.fill;
-      red_hair.fill = strk;
-      red_hair.stroke = fll;
+    case 's': {
+      let r_fll = red_hair.stroke;
+      let r_strk = red_hair.fill;
+      red_hair.fill = r_fll;
+      red_hair.stroke = r_strk;
       red_hair.update_colors(true); //update circles
       break;
     }
@@ -152,12 +172,14 @@ function keyTyped() {
   }
 
 function toggle_pan() {
-  if (document.body.style.zoom == 1.0) {
+  if (!panning) {
+    document.getElementById('menu').style.display = 'none';
     panning = true;
     svg.style.top = -windowHeight / 3;
     document.body.style.zoom = 3.0;
   } else {
     document.body.style.zoom = 1.0;
+    document.getElementById('menu').style.display = 'block';
     svg.style.left = 0;
     svg.style.top = 0;
     panning = false;
